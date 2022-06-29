570308570b95cccb35cbb44ca61ea165a842d828
Luke McDonald <luke1a.mcd+git@gmail.com>
2022-06-30 00:12:01 +1000

    let carry creep carry to extentions and towers

--- carry.js
+++ carry.js
@@ -8,16 +8,25 @@ module.exports = {
         }
 
         if (creep.memory.full) {
-            let spawn = creep.pos.findClosestByPath(FIND_MY_SPAWNS);
-            if (creep.transfer(spawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(spawn);
+            let target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION ||
+                        structure.structureType == STRUCTURE_SPAWN ||
+                        structure.structureType == STRUCTURE_TOWER) &&
+                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                }
+            });;
+            if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(target);
             }
         }
         else {
             let storage = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: { structureType: STRUCTURE_CONTAINER }
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_CONTAINER && structure.store.getUsedCapacity(RESOURCE_ENERGY))
+                }
             });
-            if(creep.withdraw(storage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            if (creep.withdraw(storage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(storage);
             }
         }
