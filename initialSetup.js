ec71487558b64061a2f6a9f093fa8914566c385c
Luke McDonald <luke1a.mcd+git@gmail.com>
2022-06-29 22:32:49 +1000

    deleted saving sources to memory

--- initialSetup.js
+++ initialSetup.js
@@ -2,7 +2,5 @@ module.exports = {
     run: function (room, name) {
         room.memory.isSetup = true;
         room.memory.hasStorage = false;
-        let sources = room.find(FIND_SOURCES)
-        room.memory.sources = sources;
     }
 }
